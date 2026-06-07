ac2a64b4d413ad0f21aaf3c5f3d06db7ab872754
gimbuk <gimbuk@DESKTOP-R7NL75T>
2026-06-08 07:48:31 +0900

    초기 커밋.

---
+++ Han/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Han
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImgBox = new System.Windows.Forms.PictureBox();
+            this.btnimgLoad = new System.Windows.Forms.Button();
+            this.btninspect = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.dgvLog = new System.Windows.Forms.DataGridView();
+            this.lblSimilarity = new System.Windows.Forms.Label();
+            this.btnTemplateLoad = new System.Windows.Forms.Button();
+            this.btnSaveROI = new System.Windows.Forms.Button();
+            this.btnLoadROI = new System.Windows.Forms.Button();
+            this.txtThreshold = new System.Windows.Forms.TextBox();
+            this.lblThreshold = new System.Windows.Forms.Label();
+            this.TemplateBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TemplateBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImgBox
+            // 
+            this.ImgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImgBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImgBox.Location = new System.Drawing.Point(1, 0);
+            this.ImgBox.Name = "ImgBox";
+            this.ImgBox.Size = new System.Drawing.Size(770, 420);
+            this.ImgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImgBox.TabIndex = 0;
+            this.ImgBox.TabStop = false;
+            this.ImgBox.Paint += new System.Windows.Forms.PaintEventHandler(this.ImgBox_Paint);
+            this.ImgBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ImgBox_MouseDown);
+            this.ImgBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ImgBox_MouseMove);
+            this.ImgBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ImgBox_MouseUp);
+            // 
+            // btnimgLoad
+            // 
+            this.btnimgLoad.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnimgLoad.Font = new System.Drawing.Font("맑은 고딕", 11F);
+            this.btnimgLoad.Location = new System.Drawing.Point(649, 514);
+            this.btnimgLoad.Name = "btnimgLoad";
+            this.btnimgLoad.Size = new System.Drawing.Size(200, 75);
+            this.btnimgLoad.TabIndex = 1;
+            this.btnimgLoad.Text = "이미지 불러오기";
+            this.btnimgLoad.UseVisualStyleBackColor = false;
+            this.btnimgLoad.Click += new System.EventHandler(this.btnimgLoad_Click);
+            // 
+            // btninspect
+            // 
+            this.btninspect.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btninspect.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btninspect.Location = new System.Drawing.Point(788, 435);
+            this.btninspect.Name = "btninspect";
+            this.btninspect.Size = new System.Drawing.Size(200, 50);
+            this.btninspect.TabIndex = 2;
+            this.btninspect.Text = "검사";
+            this.btninspect.UseVisualStyleBackColor = false;
+            this.btninspect.Click += new System.EventHandler(this.btninspect_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblResult.ForeColor = System.Drawing.Color.Blue;
+            this.lblResult.Location = new System.Drawing.Point(508, 423);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(182, 67);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "READY";
+            // 
+            // dgvLog
+            // 
+            this.dgvLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLog.Location = new System.Drawing.Point(50, 514);
+            this.dgvLog.Name = "dgvLog";
+            this.dgvLog.RowHeadersWidth = 51;
+            this.dgvLog.Size = new System.Drawing.Size(500, 200);
+            this.dgvLog.TabIndex = 4;
+            // 
+            // lblSimilarity
+            // 
+            this.lblSimilarity.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSimilarity.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblSimilarity.Location = new System.Drawing.Point(273, 439);
+            this.lblSimilarity.Name = "lblSimilarity";
+            this.lblSimilarity.Size = new System.Drawing.Size(202, 50);
+            this.lblSimilarity.TabIndex = 5;
+            this.lblSimilarity.Text = "유사도/기존유사도";
+            this.lblSimilarity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnTemplateLoad
+            // 
+            this.btnTemplateLoad.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnTemplateLoad.Font = new System.Drawing.Font("맑은 고딕", 11F);
+            this.btnTemplateLoad.Location = new System.Drawing.Point(923, 514);
+            this.btnTemplateLoad.Name = "btnTemplateLoad";
+            this.btnTemplateLoad.Size = new System.Drawing.Size(200, 75);
+            this.btnTemplateLoad.TabIndex = 6;
+            this.btnTemplateLoad.Text = "템플릿 불러오기";
+            this.btnTemplateLoad.UseVisualStyleBackColor = false;
+            this.btnTemplateLoad.Click += new System.EventHandler(this.btnTemplateLoad_Click);
+            // 
+            // btnSaveROI
+            // 
+            this.btnSaveROI.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSaveROI.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.btnSaveROI.Location = new System.Drawing.Point(649, 636);
+            this.btnSaveROI.Name = "btnSaveROI";
+            this.btnSaveROI.Size = new System.Drawing.Size(200, 75);
+            this.btnSaveROI.TabIndex = 7;
+            this.btnSaveROI.Text = "ROI저장";
+            this.btnSaveROI.UseVisualStyleBackColor = false;
+            this.btnSaveROI.Click += new System.EventHandler(this.btnSaveROI_Click);
+            // 
+            // btnLoadROI
+            // 
+            this.btnLoadROI.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLoadROI.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.btnLoadROI.Location = new System.Drawing.Point(923, 636);
+            this.btnLoadROI.Name = "btnLoadROI";
+            this.btnLoadROI.Size = new System.Drawing.Size(200, 75);
+            this.btnLoadROI.TabIndex = 8;
+            this.btnLoadROI.Text = "ROI불러오기";
+            this.btnLoadROI.UseVisualStyleBackColor = false;
+            this.btnLoadROI.Click += new System.EventHandler(this.btnLoadROI_Click);
+            // 
+            // txtThreshold
+            // 
+            this.txtThreshold.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtThreshold.Location = new System.Drawing.Point(137, 453);
+            this.txtThreshold.Name = "txtThreshold";
+            this.txtThreshold.Size = new System.Drawing.Size(100, 27);
+            this.txtThreshold.TabIndex = 9;
+            this.txtThreshold.Text = "0.80";
+            this.txtThreshold.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblThreshold
+            // 
+            this.lblThreshold.AutoSize = true;
+            this.lblThreshold.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblThreshold.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblThreshold.Location = new System.Drawing.Point(27, 456);
+            this.lblThreshold.Name = "lblThreshold";
+            this.lblThreshold.Size = new System.Drawing.Size(104, 20);
+            this.lblThreshold.TabIndex = 10;
+            this.lblThreshold.Text = "유사도 기준값";
+            // 
+            // TemplateBox
+            // 
+            this.TemplateBox.BackColor = System.Drawing.Color.Transparent;
+            this.TemplateBox.Location = new System.Drawing.Point(777, 12);
+            this.TemplateBox.Name = "TemplateBox";
+            this.TemplateBox.Size = new System.Drawing.Size(400, 400);
+            this.TemplateBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TemplateBox.TabIndex = 11;
+            this.TemplateBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.TemplateBox);
+            this.Controls.Add(this.lblThreshold);
+            this.Controls.Add(this.txtThreshold);
+            this.Controls.Add(this.btnLoadROI);
+            this.Controls.Add(this.btnSaveROI);
+            this.Controls.Add(this.btnTemplateLoad);
+            this.Controls.Add(this.lblSimilarity);
+            this.Controls.Add(this.dgvLog);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btninspect);
+            this.Controls.Add(this.btnimgLoad);
+            this.Controls.Add(this.ImgBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TemplateBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImgBox;
+        private System.Windows.Forms.Button btnimgLoad;
+        private System.Windows.Forms.Button btninspect;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dgvLog;
+        private System.Windows.Forms.Label lblSimilarity;
+        private System.Windows.Forms.Button btnTemplateLoad;
+        private System.Windows.Forms.Button btnSaveROI;
+        private System.Windows.Forms.Button btnLoadROI;
+        private System.Windows.Forms.TextBox txtThreshold;
+        private System.Windows.Forms.Label lblThreshold;
+        private System.Windows.Forms.PictureBox TemplateBox;
+    }
+}
+
---
+++ Han/Form1.cs
@@ -0,0 +1,265 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Point = System.Drawing.Point;
+
+namespace Han
+{
+    public partial class Form1 : Form
+    {
+        // ROI저장,이미지 경로
+        private string currentImagePath = "";
+        private string templateImagePath = "";
+        private bool isDragging = false;
+        private Point startPoint;
+        private Rectangle roiRect;
+        private readonly string csvPath =
+           Path.Combine(Application.StartupPath, "inspection_log.csv");
+        private readonly string roiPath =
+           Path.Combine(Application.StartupPath, "roi.txt");
+        public Form1()
+        {
+            InitializeComponent();
+            // 검사 결과 로그
+            dgvLog.Columns.Add("Time", "시간");
+            dgvLog.Columns.Add("FileName", "파일명");
+            dgvLog.Columns.Add("Similarity", "유사도");
+            dgvLog.Columns.Add("Result", "결과");
+            // CSV파일 없으면 헤더 생성
+            if (!File.Exists(csvPath))
+            {
+                File.WriteAllText(
+                    csvPath,
+                    "시간,파일명,유사도,결과" + Environment.NewLine,
+                    Encoding.UTF8
+                );
+            }
+
+        }
+        private void btnimgLoad_Click(object sender, EventArgs e)   // 이미지 로드
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "이미지 파일|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                currentImagePath = ofd.FileName;
+                ImgBox.Image = Image.FromFile(currentImagePath);
+            }
+        }
+        private void btninspect_Click(object sender, EventArgs e)  // 유사도 검사
+        {
+            if (string.IsNullOrEmpty(templateImagePath))
+            {
+                MessageBox.Show("템플릿 이미지를 먼저 불러오세요.");
+                return;
+            }
+            if (ImgBox.Image == null)
+            {
+                MessageBox.Show("이미지를 먼저 불러오세요.");
+                return;
+            }
+
+            if (roiRect.Width <= 0 || roiRect.Height <= 0)
+            {
+                MessageBox.Show("ROI를 먼저 선택하세요.");
+                return;
+            }
+
+            Mat img = Cv2.ImRead(currentImagePath);
+
+            // PictureBox -> 이미지 좌표 변환
+            double scaleX = (double)img.Width / ImgBox.Width;
+            double scaleY = (double)img.Height / ImgBox.Height;
+
+            Rect roi = new Rect(
+                (int)(roiRect.X * scaleX),
+                (int)(roiRect.Y * scaleY),
+                (int)(roiRect.Width * scaleX),
+                (int)(roiRect.Height * scaleY)
+            );
+            // ROI 좌표 음수방지
+            roi.X = Math.Max(0, roi.X);
+            roi.Y = Math.Max(0, roi.Y);
+
+            roi.Width = Math.Min(roi.Width, img.Width - roi.X);
+            roi.Height = Math.Min(roi.Height, img.Height - roi.Y);
+
+            Mat roiImage = new Mat(img, roi);
+            Mat gray = new Mat();                                                         // 흑백 변환
+            Cv2.CvtColor(roiImage, gray, ColorConversionCodes.BGR2GRAY);
+            Mat template = Cv2.ImRead(templateImagePath);
+            Mat templateGray = new Mat();
+            Cv2.CvtColor(template, templateGray, ColorConversionCodes.BGR2GRAY);
+            // ROI 크기에 맞게 템플릿 리사이즈
+            Cv2.Resize(
+                templateGray,
+                templateGray,
+                new OpenCvSharp.Size(gray.Width, gray.Height)
+                      );
+            Mat resultMat = new Mat();
+            // 유사도 계산
+            Cv2.MatchTemplate(
+                gray,
+                templateGray,
+                resultMat,
+                TemplateMatchModes.CCoeffNormed
+            );
+
+            Cv2.MinMaxLoc(
+                resultMat,
+                out double minVal,
+                out double maxVal,
+                out _,
+                out _
+            );
+            double similarity = maxVal;
+            double threshold;
+
+            if (!double.TryParse(txtThreshold.Text, out threshold))
+            {
+                MessageBox.Show("유사도 기준값을 숫자로 입력하세요.");
+                return;
+            }
+            string result;
+            // 유사도 판정
+            if (similarity >= threshold)
+            {
+                result = "PASS";
+            }
+            else
+            {
+                result = "FAIL";
+            }
+            if (result == "PASS")
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "● Pass";
+            }
+            else
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "● Fail";
+
+                // FAIL 이미지 저장
+                string failFolder = Path.Combine(Application.StartupPath, "FailImages");
+                Cv2.ImWrite("roi_debug.png", roiImage);
+                Cv2.ImWrite("template_debug.png", template);
+
+                Directory.CreateDirectory(failFolder);
+
+                string fileName = Path.GetFileNameWithoutExtension(currentImagePath);
+                string extension = Path.GetExtension(currentImagePath);
+                string savePath = Path.Combine(
+                    failFolder,
+                    $"{DateTime.Now:yyyyMMdd_HHmmss}_{fileName}{extension}"
+                );
+
+                // ROI 영역 표시
+                Cv2.Rectangle(
+                    img,
+                    roi,
+                    new Scalar(0, 0, 255),
+                    3  // 선 두께
+                );
+                Cv2.ImWrite(savePath, img);
+
+                MessageBox.Show($"FAIL 이미지 저장됨\n{savePath}");
+            }
+            
+            lblSimilarity.Text = $"유사도 : {similarity:F3}\r\n기준 : {threshold:F2}";
+
+            string logFileName = Path.GetFileName(currentImagePath);
+            // 화면 로그
+            dgvLog.Rows.Add(
+                DateTime.Now.ToString("HH:mm:ss"),
+                logFileName,
+                similarity.ToString("F2"),
+                result
+            );
+            // CSV 검사 이력 저장
+            File.AppendAllText(
+            csvPath,
+            $"{DateTime.Now:HH:mm:ss},{logFileName},{similarity:F3},{result}{Environment.NewLine}",
+            Encoding.UTF8
+            );
+        }
+        private void ImgBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            startPoint = e.Location;
+        }
+        private void ImgBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDragging)
+                return;
+            roiRect = new Rectangle(
+                Math.Min(startPoint.X, e.X),
+                Math.Min(startPoint.Y, e.Y),
+                Math.Abs(e.X - startPoint.X),
+                Math.Abs(e.Y - startPoint.Y)
+            );
+            ImgBox.Invalidate();
+        }
+        private void ImgBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+        private void ImgBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (roiRect.Width > 0 && roiRect.Height > 0)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, roiRect);
+            }
+        }
+        private void btnTemplateLoad_Click(object sender, EventArgs e) // 템플릿 로드
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "이미지 파일|*.jpg;*.jpeg;*.png;*.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                templateImagePath = ofd.FileName;
+                TemplateBox.Image =
+                   Image.FromFile(templateImagePath);
+
+                MessageBox.Show("템플릿 이미지 로드 완료");
+            }
+        }
+        private void btnSaveROI_Click(object sender, EventArgs e) // ROI 좌표 저장
+        {
+            File.WriteAllText(
+                           roiPath,
+                            $"{roiRect.X},{roiRect.Y},{roiRect.Width},{roiRect.Height}"
+              );
+            MessageBox.Show("ROI 저장 완료");
+        }
+        private void btnLoadROI_Click(object sender, EventArgs e) // ROI 좌표 불러오기
+        {
+            if (!File.Exists(roiPath))
+            {
+                MessageBox.Show("저장된 ROI가 없습니다.");
+                return;
+            }
+
+            string[] data = File.ReadAllText(roiPath).Split(',');
+
+            roiRect = new Rectangle(
+                int.Parse(data[0]),
+                int.Parse(data[1]),
+                int.Parse(data[2]),
+                int.Parse(data[3])
+            );
+            ImgBox.Invalidate();
+            MessageBox.Show("ROI 불러오기 완료");
+        }
+    }
+}
